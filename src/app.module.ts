aa71bcc571fff3950db8e18980cc32869ffffaf1
Eugene M <eugene.moschyn@gmail.com>
2022-09-24 20:15:38 -0700

    add(): import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { Models3DModule } from './models-3d/models-3d.module';
 
 
 @Module({
     imports: [
         PrismaModule,
+        Models3DModule,
     ]
 })
 export class AppModule {}

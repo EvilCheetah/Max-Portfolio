9b82045c30c3395fd31dea5a27fa79c5ef7b2507
Eugene M <eugene.moschyn@gmail.com>
2022-09-27 01:51:18 -0700

    add(): import of user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { Models3DModule } from './models-3d/models-3d.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
     imports: [
         PrismaModule,
         Models3DModule,
+        UsersModule,
     ]
 })
 export class AppModule {}

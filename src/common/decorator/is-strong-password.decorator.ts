c595ba4acae44680dff3485f3b79be504b50bac5
Eugene M <eugene.moschyn@gmail.com>
2022-09-30 00:17:48 -0700

    create(): function and decorator for strong passwd

---
+++ src/common/decorator/is-strong-password.decorator.ts
@@ -0,0 +1,31 @@
+import { buildMessage, isString, matches, ValidateBy, ValidationArguments, ValidationOptions } from "class-validator";
+import { STRONG_PASSWORD } from "../constant/strong-password.constant";
+
+
+export function isStrongPassword(value: unknown): boolean
+{
+    return isString(value) && 
+            matches(value, STRONG_PASSWORD);
+}
+
+
+export function IsStrongPassword(validation_options?: ValidationOptions)
+{
+    return ValidateBy({
+        name: 'IsStrongPassword',
+        validator: {
+            validate: (value: any, args: ValidationArguments): boolean => isStrongPassword(value),
+            defaultMessage: buildMessage(
+                each_prefix => (
+                    each_prefix + '$property must be a strong password. Must follow the following criteria: ' +
+                    'must be between 8 and 120 characters long, '                                             +
+                    'must have at least 1 uppercase letter, '                                                 + 
+                    'must have at least 1 lowercase letter, '                                                 +
+                    'must have at least 1 digit, '                                                            + 
+                    'one of the following special characters - @$!%*?&'
+                ),
+                validation_options
+            )
+        }
+    })
+}

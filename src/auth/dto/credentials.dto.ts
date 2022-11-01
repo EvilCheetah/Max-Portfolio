a8d868ae1008d141a7ee2f70401ca6a7377c448f
Eugene M <eugene.moschyn@gmail.com>
2022-11-01 00:55:59 -0700

    add(): same constraints as in other dtos for username and password

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,13 +1,16 @@
-import { IsString } from "class-validator";
+import { IsString, MaxLength } from "class-validator";
 
 import { IsUsername } from "@decorator";
+import { MAX_ALLOWED_PASSWORD_LENGTH, MAX_ALLOWED_USERNAME_LENGTH } from "@constant";
 
 
 export class CredentialsDTO
 {
     @IsUsername()
+    @MaxLength( MAX_ALLOWED_USERNAME_LENGTH )
     username: string;
 
     @IsString()
+    @MaxLength( MAX_ALLOWED_PASSWORD_LENGTH )
     password: string;
 }

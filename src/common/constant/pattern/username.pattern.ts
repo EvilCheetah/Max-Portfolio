677af6f1e509baa361292e45942c8a482336a0f6
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2022-11-04 20:44:30 -0700

    remove(): global flag to avoid unexpected behavior

--- src/common/constant/pattern/username.pattern.ts
+++ src/common/constant/pattern/username.pattern.ts
@@ -2,4 +2,4 @@
 /// 1) Start with Letter
 /// 2) Then allowed characters: A-Z, a-z, 0-9 and '_'
 
-export const USERNAME = /^[a-zA-Z][a-zA-Z\d_]+$/g
+export const USERNAME = /^[a-zA-Z][a-zA-Z\d_]+$/

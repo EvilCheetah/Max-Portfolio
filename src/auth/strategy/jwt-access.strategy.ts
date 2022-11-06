dced4940a8691c95aa20623aa45bf3acac7b4e11
Eugene M <eugene.moschyn@gmail.com>
2022-11-05 22:48:59 -0700

    add(): jwt strategy for access token

---
+++ src/auth/strategy/jwt-access.strategy.ts
@@ -0,0 +1,26 @@
+import { JwtPayload } from "@interface";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "src/users/users.service";
+
+
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt-access')
+{
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly usersService:  UsersService
+    )
+    {
+        super({
+            jwtFromRequest:   ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey:      configService.get('JWT_ACCESS_TOKEN_SECRET')
+        });
+    }
+
+    validate(validation_payload: JwtPayload)
+    {
+        return this.usersService.findOneBy({ email: validation_payload?.email })
+    }
+}

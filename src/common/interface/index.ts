e0f74493cf7b530773207b88004d3f9528395dfe
Eugene M <eugene.moschyn@gmail.com>
2022-11-05 21:39:37 -0700

    add(): jwt interfaces to export

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -1,1 +1,6 @@
+export * from './jwt/access-token.interface';
+export * from './jwt/jwt-payload.interface';
+export * from './jwt/jwt-tokens.interface';
+export * from './jwt/refresh-token.interface';
+
 export * from './unique-user-criteria.interface';

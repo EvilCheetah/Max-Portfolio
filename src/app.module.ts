75af226f5142d6739aba26317621f60c81d3c001
Eugene M <eugene.moschyn@gmail.com>
2022-11-05 21:09:25 -0700

    add(): joi validation schema to config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { validationSchema } from '@config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { Models3DModule } from './models-3d/models-3d.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
+        ConfigModule.forRoot({ isGlobal: true, validationSchema }),
         PrismaModule,
         
         Models3DModule,

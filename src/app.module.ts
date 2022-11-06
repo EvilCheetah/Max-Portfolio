8ffa5b13c649bbfbf5acbd50fdce966d28b44380
Eugene M <eugene.moschyn@gmail.com>
2022-11-05 19:25:53 -0700

    add(): config-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { Models3DModule } from './models-3d/models-3d.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule,
         
         Models3DModule,

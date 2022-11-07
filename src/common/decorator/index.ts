f9475acfd2a1e08d82423189dde58d4d9ede4bb2
Eugene M <eugene.moschyn@gmail.com>
2022-11-06 17:50:01 -0800

    add(): user-related decorators to export

--- src/common/decorator/index.ts
+++ src/common/decorator/index.ts
@@ -1,3 +1,6 @@
+export * from './parameter/user.decorator';
+export * from './parameter/get-user.decorator';
+
 export * from './validator/is-equal-to.decorator';
 export * from './validator/is-username.decorator';
 export * from './validator/is-strong-password.decorator';

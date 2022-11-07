e124122cb8a741ede7f761a66bd08511e6bd2292
Eugene M <eugene.moschyn@gmail.com>
2022-11-07 00:37:19 -0800

    update(): length constraints with const values

--- src/common/decorator/validator/is-strong-password.decorator.ts
+++ src/common/decorator/validator/is-strong-password.decorator.ts
@@ -1,6 +1,6 @@
 import { buildMessage, isString, matches, ValidateBy, ValidationArguments, ValidationOptions } from "class-validator";
 
-import { STRONG_PASSWORD } from "@constant";
+import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH, STRONG_PASSWORD } from "@constant";
 
 
 export function isStrongPassword(value: unknown): boolean
@@ -19,7 +19,7 @@ export function IsStrongPassword(validation_options?: ValidationOptions)
             defaultMessage: buildMessage(
                 each_prefix => (
                     each_prefix + '$property must be a strong password. Must follow the following criteria: ' +
-                    'must be between 8 and 120 characters long, '                                             +
+                    `must be between ${MIN_PASSWORD_LENGTH} and ${MAX_PASSWORD_LENGTH} characters long, `     +
                     'must have at least 1 uppercase letter, '                                                 + 
                     'must have at least 1 lowercase letter, '                                                 +
                     'must have at least 1 digit, '                                                            + 

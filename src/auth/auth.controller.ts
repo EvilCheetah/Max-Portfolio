bf998cc5edfc78e5cd2b2dd81d2d8d018f78653c
Eugene M <eugene.moschyn@gmail.com>
2022-11-06 18:17:19 -0800

    update(): several things listed below 1) Added http-codes on routes 2) Updated guards used on end points 3) Routed the controller functions to service functions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 
+import { Token } from '@types';
+import { JwtTokens } from '@interface';
 import { NewUserDTO } from '@dto';
+import { GetUser, Public, User } from '@decorator';
+import { JwtRefreshAuthGuard, LocalAuthGuard } from '@guard';
 import { AuthService } from './auth.service';
 import { CredentialsDTO } from './dto/credentials.dto';
 
@@ -10,33 +14,52 @@ export class AuthController
 {
     constructor(private readonly authService: AuthService) {}
 
+
+    @Public()
     @Post('signup')
     signup(
         @Body()
         information: NewUserDTO
-    )
+    ): Promise<JwtTokens>
     {
         return this.authService.signup(information);
     }
 
+
     @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(LocalAuthGuard)
     login(
-        @Body()
+        @User()
         credentials: CredentialsDTO
-    )
+    ): Promise<JwtTokens>
     {
-        
+        return this.authService.login(credentials);
     }
+
 
     @Post('logout')
-    logout()
+    @HttpCode(HttpStatus.OK)
+    logout(
+        @GetUser('sub', ParseIntPipe)
+        user_id: number
+    ): Promise<string>
     {
-
+        return this.authService.logout(user_id);
     }
 
+
     @Post('refresh')
-    refresh()
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(JwtRefreshAuthGuard)
+    refresh(
+        @GetUser('sub', ParseIntPipe)
+        user_id: number,
+
+        @GetUser('refresh_token')
+        refresh_token: Token
+    ): Promise<JwtTokens>
     {
-        
+        return this.authService.refresh(user_id, refresh_token);
     }
 }

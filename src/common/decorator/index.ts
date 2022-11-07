30e2fa03242238765aec77937ab9e72dece0804d
Eugene M <eugene.moschyn@gmail.com>
2022-11-06 17:51:05 -0800

    add(): public decorator to export

--- src/common/decorator/index.ts
+++ src/common/decorator/index.ts
@@ -1,6 +1,8 @@
 export * from './parameter/user.decorator';
 export * from './parameter/get-user.decorator';
 
+export * from './router/public.decorator';
+
 export * from './validator/is-equal-to.decorator';
 export * from './validator/is-username.decorator';
 export * from './validator/is-strong-password.decorator';
